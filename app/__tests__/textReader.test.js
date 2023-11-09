27bd75b7a1fc9080b60cb6ca84bd8ad6814b3f70
neottil <neot.luka.89@gmail.com>
2023-11-09 15:19:15 +0100

    fix test

--- app/__tests__/textReader.test.js
+++ app/__tests__/textReader.test.js
@@ -1,12 +1,12 @@
 import { readText, makeFileStatistics } from "../textReader.js";
 
 test("Make statistics", () => {
-  readText("./testFile.txt").then((text) => {
+  return readText("./__tests__/testFile.txt").then((text) => {
     const { wordCount, letterCount, spaceCount, repeatedWords } =
       makeFileStatistics(text);
-    expect(wordCount.toBe(88));
-    expect(letterCount.toBe(407));
-    expect(spaceCount.toBe(74));
-    expect(repeatedWords.toEquals({ armonia: 10, l: 11 }));
+    expect(wordCount).toBe(88);
+    expect(letterCount).toBe(407);
+    expect(spaceCount).toBe(74);
+    expect(repeatedWords).toEqual({ armonia: 10, l: 11 });
   });
 });
